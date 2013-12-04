97cc5c6f25380e5792d15ccd3310b7f7f35f8e55
Zach <zacharyabyrne@gmail.com>
2013-12-03 20:40:45 -0700

    z80: Add z80_reset function and test.

--- z80/test_z80.c
+++ z80/test_z80.c
@@ -1,8 +1,34 @@
+#include <stdint.h>
 #include <check.h>
+#include <string.h>
+#include "z80.h"
 
+/*Tests look like this
 START_TEST(dummy_test)
 {
     ck_assert_int_eq(5, 5);
+}
+END_TEST
+*/
+
+START_TEST(test_z80_reset)
+{
+    Z80_t proc;
+    memset(&proc, 0xFF, sizeof(proc));
+    z80_reset(&proc);
+    ck_assert_int_eq(proc.registers.a, 0);
+    ck_assert_int_eq(proc.registers.b, 0);
+    ck_assert_int_eq(proc.registers.c, 0);
+    ck_assert_int_eq(proc.registers.d, 0);
+    ck_assert_int_eq(proc.registers.e, 0);
+    ck_assert_int_eq(proc.registers.f, 0);
+    ck_assert_int_eq(proc.registers.pc, 0);
+    ck_assert_int_eq(proc.registers.sp, 0);
+    ck_assert_int_eq(proc.registers.m, 0);
+    ck_assert_int_eq(proc.registers.t, 0);
+    ck_assert_int_eq(proc.registers.ime, 1);
+    ck_assert_int_eq(proc.clocks.m, 0);
+    ck_assert_int_eq(proc.clocks.t, 0);
 }
 END_TEST
 
@@ -11,7 +37,11 @@ z80_suite(void)
 {
     Suite *s = suite_create("z80");
     TCase *tc_core = tcase_create("Core");
-    tcase_add_test(tc_core, dummy_test);
+/*
+  And are added here, like this
+    tcase_add_test(tc_core, test_z80_reset);
+ */
+    tcase_add_test(tc_core, test_z80_reset);
     suite_add_tcase(s, tc_core);
     return s;
 }
--- z80/z80.c
+++ z80/z80.c
@@ -1,2 +1,25 @@
 #include <stdint.h>
 #include "z80.h"
+
+/*
+ * Reset the z80 struct to power on state.
+ */
+void
+z80_reset(pZ80_t proc)
+{
+    proc->registers.a = 0;
+    proc->registers.b = 0;
+    proc->registers.c = 0;
+    proc->registers.d = 0;
+    proc->registers.e = 0;
+    proc->registers.h = 0;
+    proc->registers.l = 0;
+    proc->registers.f = 0;
+    proc->registers.pc = 0;
+    proc->registers.sp = 0;
+    proc->registers.m = 0;
+    proc->registers.t = 0;
+    proc->registers.ime = 1;
+    proc->clocks.m = 0;
+    proc->clocks.t = 0;
+}
