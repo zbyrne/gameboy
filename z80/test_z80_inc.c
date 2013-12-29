8cceb07c36096ac44c452f2b1610a3cd0b4495ce
Zach <zacharyabyrne@gmail.com>
2013-12-28 22:09:11 -0700

    z80 test: Fixed some leftover syntax from sample code.

--- z80/test_z80_inc.c
+++ z80/test_z80_inc.c
@@ -90,10 +90,10 @@ int
 main(void)
 {
     int number_failed;
-    Suite *s = z80_inc_suite ();
-    SRunner *sr = srunner_create (s);
-    srunner_run_all (sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed (sr);
-    srunner_free (sr);
+    Suite *s = z80_inc_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
     return number_failed;
 }
--- z80/test_z80_ld.c
+++ z80/test_z80_ld.c
@@ -105,10 +105,10 @@ int
 main(void)
 {
     int number_failed;
-    Suite *s = z80_ld_suite ();
-    SRunner *sr = srunner_create (s);
-    srunner_run_all (sr, CK_NORMAL);
-    number_failed = srunner_ntests_failed (sr);
-    srunner_free (sr);
+    Suite *s = z80_ld_suite();
+    SRunner *sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
     return number_failed;
 }
