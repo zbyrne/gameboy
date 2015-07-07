40650f9de575c03e3a13c7e12c320bfd67e567e4
Zach <zacharyabyrne@gmail.com>
2015-07-06 21:14:06 -0600

    PYBOY: Add the first set of instructions.

--- pyboy/z80.py
+++ pyboy/z80.py
@@ -86,3 +86,74 @@ class Z80(object):
             self.f |= C_FLAG
         else:
             self.f &= ~C_FLAG
+
+    # Instructions
+    # Return clock cycles used.
+    # Each instruction is responsible for updating PC.
+    # This is to make branching simpler.
+
+    def nop(self):
+        self.pc += 1
+        return 4
+
+    def ld_bc_d16(self):
+        self.pc += 1
+        self.bc = self._mem.read_word(self.pc)
+        self.pc += 2
+        return 12
+
+    def ld_addr_bc_a(self):
+        self._mem.write_byte(self.a, self.bc)
+        self.pc +=1
+        return 8
+
+    def inc_bc(self):
+        self.bc += 1
+        self.pc += 1
+        return 8
+
+    def inc_b(self):
+        self.h_flag = ((self.b & 0xF) + 1) > 0xF
+        self.b = (self.b + 1) & 0xFF
+        self.z_flag = self.b == 0
+        self.n_flag = False
+        self.pc += 1
+        return 4
+
+    def dec_b(self):
+        self.h_flag = (self.b & 0xF0) > ((self.b - 1) & 0xF0)
+        self.b = 0xFF if self.b == 0 else (self.b - 1) & 0xFF
+        self.z_flag = self.b == 0
+        self.n_flag = True
+        self.pc += 1
+        return 4
+
+    def ld_b_d8(self):
+        self.pc += 1
+        self.b = self._mem.read_byte(self.pc)
+        self.pc += 1
+        return 8
+
+    def rlca(self):
+        self.pc +=1
+        self.f = 0
+        self.c_flag = bool(self.a & (1<<7))
+        self.a <<= 1
+        self.a &= 0xFF
+        self.a += int(self.c_flag)
+        return 4
+
+    def ld_a16_sp(self):
+        self.pc +=1
+        addr = self._mem.read_word(self.pc)
+        self.pc += 2
+        self._mem.write_word(self.sp, addr)
+        return 20
+
+    def add_hl_bc(self):
+        self.pc += 1
+        self.n_flag = False
+        self.c_flag = (self.hl + self.bc) > 0xFFFF
+        self.h_flag = ((self.hl & 0xFFF) + (self.bc & 0xFFF)) > 0xFFF
+        self.hl = (self.hl + self.pc) & 0xFFFF
+        return 8
