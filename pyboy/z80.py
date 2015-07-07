b6c87033af396246326e132dbc0d0aa051d07767
Zach <zacharyabyrne@gmail.com>
2015-07-06 20:23:22 -0600

    PYBOY: Add properties for status bits.

--- pyboy/z80.py
+++ pyboy/z80.py
@@ -1,6 +1,12 @@
+Z_FLAG = 1 << 7
+N_FLAG = 1 << 6
+H_FLAG = 1 << 5
+C_FLAG = 1 << 4
+
 class Z80(object):
 
-    def __init__(self):
+    def __init__(self, mem):
+        self._mem = mem
         self.a = 0
         self.b = 0
         self.c = 0
@@ -36,3 +42,47 @@ class Z80(object):
     def set(self, val):
         self.h = (val >> 8) & 0xFF
         self.l = val & 0xFF
+
+    @property
+    def z_flag(self):
+        return bool(self.f & Z_FLAG)
+
+    @z_flag.setter
+    def set(self, val):
+        if val:
+            self.f |= Z_FLAG
+        else:
+            self.f &= ~Z_FLAG
+
+    @property
+    def n_flag(self):
+        return bool(self.f & N_FLAG)
+
+    @n_flag.setter
+    def set(self, val):
+        if val:
+            self.f |= N_FLAG
+        else:
+            self.f &= ~N_FLAG
+
+    @property
+    def h_flag(self):
+        return bool(self.f & H_FLAG)
+
+    @h_flag.setter
+    def set(self, val):
+        if val:
+            self.f |= H_FLAG
+        else:
+            self.f &= ~H_FLAG
+
+    @property
+    def c_flag(self):
+        return bool(self.f & C_FLAG)
+
+    @c_flag.setter
+    def set(self, val):
+        if val:
+            self.f |= C_FLAG
+        else:
+            self.f &= ~C_FLAG
