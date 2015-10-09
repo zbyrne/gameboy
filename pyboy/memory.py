767cc3c747a0fb05c423e60aeec1afa7d66c0d90
Zach <zacharyabyrne@gmail.com>
2015-10-08 20:39:42 -0600

    PYBOY: Rewrite MemoryController to plug in mapped controllers.
    The main memory controller will maintain a list of
    sub-controllers that will manage RAM, cartridges, IO registers,
    video RAM, etc.
    
    A simple ram controller(basically just a list) is included for
    use in unit tests.

--- pyboy/memory.py
+++ pyboy/memory.py
@@ -1,22 +1,45 @@
+from collections import namedtuple
+
+
+MappedController = namedtuple('MappedController',
+                              ('controller', 'start', 'length'))
+
+
 class MemoryController(object):
-    def __init__(self, cart_path):
-        with open(cart_path, 'rb+') as cart:
-            rom = cart.read()
-        rom_bank = [ord(x) for x in rom]
-        ram_bank = [0]*0x8000
-        self.memory = rom_bank + ram_bank
+    def __init__(self):
+        self._memory_map = []
+
+    def register_controller(self, controller, start):
+        con = MappedController(controller, start, len(controller))
+        self._memory_map.append(con)
+
+    def _get_controller(self, addr):
+        for con in self._memory_map:
+            if addr >= con.start:
+                if addr - con.start < con.length:
+                    return con
+        raise IndexError("memory out of range: 0x%x" % addr)
 
     def read_byte(self, addr):
-        return self.memory[addr]
+        con = self._get_controller(addr)
+        return con.controller[addr - con.start]
 
     def write_byte(self, val, addr):
-        self.memory[addr] = val
+        con = self._get_controller(addr)
+        con.controller[addr - con.start] = val
 
     def read_word(self, addr):
-        l = self.memory[addr]
-        h = self.memory[addr + 1]
+        con = self._get_controller(addr)
+        l = con.controller[addr - con.start]
+        h = con.controller[addr - con.start + 1]
         return (h << 8) + l
 
     def write_word(self, val, addr):
-        self.memory[addr] = val & 0xFF
-        self.memory[addr + 1] = (val >> 8) & 0xFF
+        con = self._get_controller(addr)
+        con.controller[addr - con.start] = val & 0xFF
+        con.controller[addr - con.start + 1] = (val >> 8) & 0xFF
+
+
+class RamController(list):
+    def __init__(self, size):
+        self.extend([0]*size)
