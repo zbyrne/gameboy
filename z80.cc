a7360fb0cd904976571d3f11b36f0519d6694627
Zach Byrne <zachary.byrne@emc.com>
2015-12-15 17:39:44 -0700

    Move Z80 class definition to header file.

--- z80.cc
+++ z80.cc
@@ -1,45 +1,10 @@
 #include <cstdint>
+#include "z80.h"
 
 constexpr uint8_t Z_FLAG = 1 << 7;
 constexpr uint8_t N_FLAG = 1 << 6;
 constexpr uint8_t H_FLAG = 1 << 5;
 constexpr uint8_t C_FLAG = 1 << 4;
-
-class Z80 {
-    union {
-        uint16_t reg_af;
-        struct {
-            uint8_t reg_f;
-            uint8_t reg_a;
-        };
-    };
-    union {
-        uint16_t reg_bc;
-        struct {
-            uint8_t reg_c;
-            uint8_t reg_b;
-        };
-    };
-    union {
-        uint16_t reg_de;
-        struct {
-            uint8_t reg_e;
-            uint8_t reg_d;
-        };
-    };
-    union {
-        uint16_t reg_hl;
-        struct {
-            uint8_t reg_l;
-            uint8_t reg_h;
-        };
-    };
-    uint16_t pc;
-    uint8_t add_8bit(uint8_t& a, uint8_t b, uint8_t c);
-    uint8_t sub_8bit(uint8_t& a, uint8_t b, uint8_t c);
-public:
-    Z80() : reg_af(0), reg_bc(0), reg_de(0), reg_hl(0), pc(0) {}
-};
 
 uint8_t
 Z80::add_8bit(uint8_t& a, uint8_t b, uint8_t c) {
---
+++ z80.h
@@ -0,0 +1,41 @@
+#include <cstdint>
+#ifndef Z80_H
+#define Z80_H
+
+class Z80 {
+    union {
+        uint16_t reg_af;
+        struct {
+            uint8_t reg_f;
+            uint8_t reg_a;
+        };
+    };
+    union {
+        uint16_t reg_bc;
+        struct {
+            uint8_t reg_c;
+            uint8_t reg_b;
+        };
+    };
+    union {
+        uint16_t reg_de;
+        struct {
+            uint8_t reg_e;
+            uint8_t reg_d;
+        };
+    };
+    union {
+        uint16_t reg_hl;
+        struct {
+            uint8_t reg_l;
+            uint8_t reg_h;
+        };
+    };
+    uint16_t pc;
+    uint8_t add_8bit(uint8_t& a, uint8_t b, uint8_t c);
+    uint8_t sub_8bit(uint8_t& a, uint8_t b, uint8_t c);
+public:
+    Z80() : reg_af(0), reg_bc(0), reg_de(0), reg_hl(0), pc(0) {}
+};
+
+#endif
