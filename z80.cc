82757e5610cc9e4a9822c976d07ad9c5cff58929
Zach Byrne <zachary.byrne@emc.com>
2015-12-15 17:38:10 -0700

    Flip 8 bit placement in structs.

--- z80.cc
+++ z80.cc
@@ -9,29 +9,29 @@ class Z80 {
     union {
         uint16_t reg_af;
         struct {
-            uint8_t reg_a;
             uint8_t reg_f;
+            uint8_t reg_a;
         };
     };
     union {
         uint16_t reg_bc;
         struct {
-            uint8_t reg_b;
             uint8_t reg_c;
+            uint8_t reg_b;
         };
     };
     union {
         uint16_t reg_de;
         struct {
-            uint8_t reg_d;
             uint8_t reg_e;
+            uint8_t reg_d;
         };
     };
     union {
         uint16_t reg_hl;
         struct {
-            uint8_t reg_h;
             uint8_t reg_l;
+            uint8_t reg_h;
         };
     };
     uint16_t pc;
